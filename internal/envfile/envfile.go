5034f76c397d9f54614e625f5d9225818b3cabad
8linkz <90318259+8linkz@users.noreply.github.com>
2026-07-09 09:08:59 +0200

    fix(envfile): check error returns in WriteAtomic

--- internal/envfile/envfile.go
+++ internal/envfile/envfile.go
@@ -112,9 +112,9 @@ func WriteAtomic(path string, data []byte, perm os.FileMode) error {
 		return err
 	}
 	tmpName := tmp.Name()
-	defer os.Remove(tmpName)
+	defer func() { _ = os.Remove(tmpName) }()
 	if _, err := tmp.Write(data); err != nil {
-		tmp.Close()
+		_ = tmp.Close()
 		return err
 	}
 	if err := tmp.Close(); err != nil {
